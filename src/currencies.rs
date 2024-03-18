83623cadbcf7e5dea2b5f61db4ad04dde29eaa49
lunarW1TCH <lunarw1tch@proton.me>
2024-03-18 15:21:11 +0100

    feat: implemented reading the csv file provided by the api service

--- src/currencies.rs
+++ src/currencies.rs
@@ -1,5 +1,22 @@
-struct Currency {}
+use serde::{Deserialize, Serialize};
+use std::error::Error;
 
-fn read_currencies() {
-    // TODO - https://docs.rs/csv/latest/csv/tutorial/index.html
+const CURRENCIES_PATH: &str = "./api_data/physical_currency_list.csv";
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Currency {
+    code: String,
+    name: String,
+}
+
+pub async fn read_currencies() -> Result<Vec<Currency>, Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(CURRENCIES_PATH)?;
+    let mut currencies: Vec<Currency> = Vec::new();
+
+    for result in reader.deserialize() {
+        let record: Currency = result?;
+        currencies.push(record);
+    }
+
+    Ok(currencies)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::error::Error;
+
 mod currencies;
 mod http;
 
+// TODO: enum for error types
+
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let client = reqwest::Client::new();
+
+    let data = currencies::read_currencies().await?;
+    println!("{:#?}", data);
 
     Ok(())
 }
